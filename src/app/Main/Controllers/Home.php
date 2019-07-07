e1dfbaace9211d727a8a770b56caa27f8084c1b1
Théo Huchard <theo.huchard@gmail.com>
2019-07-07 22:03:05 +0200

    Change settings.json to match current localhost

--- src/app/Main/Controllers/Home.php
+++ src/app/Main/Controllers/Home.php
@@ -3,10 +3,13 @@
 namespace Main\Controllers;
 
 use Core\Resources\App;
+use Main\Models\User;
 
 class Home{
 
     public function start() {
+        $u = new User();
+        $u->test();
         App::display('home');
     }
 
--- src/app/Main/Models/User.php
+++ src/app/Main/Models/User.php
@@ -2,10 +2,14 @@
 
 namespace Main\Models;
 
+use Core\Resources\Database;
+
 class User{
 
     public function test(){
-        
+        $db = new Database();
+        $result = $db->query("SELECT * FROM notes_users", null);
+        //$db->execute("INSERT INTO notes_users VALUES (?,?,?)", [1,2,20]);
     }
 
 }
