ab8b54c83cc10afc31af96f70de182838252e0bc
Théo Huchard <theo.huchard@gmail.com>
2019-07-11 16:06:59 +0200

    Update Database.php, settings.json, Queries.php

--- src/core/resources/Database.php
+++ src/core/resources/Database.php
@@ -12,7 +12,6 @@ class Database
     private $db_host;
     private $db_name;
     private $db_charset;
-    private $db_port;
     private $db_username;
     private $db_password;
 
@@ -21,16 +20,21 @@ class Database
      */
     public function __construct()
     {
-        $this->db_host = "mysql";
-        $this->db_name = "blog_io";
-        $this->db_charset = "utf8";
-        //$this->db_port = "3306";
-        $this->db_username = "root";
-        $this->db_password = "mysql";
+        if (sizeof(func_get_args()) == 5) {
+            $this->db_host = func_get_arg(0);
+            $this->db_name = func_get_arg(1);
+            $this->db_charset = func_get_arg(2);
+            $this->db_username = func_get_arg(3);
+            $this->db_password = func_get_arg(4);
+        } else {
+            $this->db_host = Settings::getSettings()->{'database'}->{'db_host'};
+            $this->db_name = Settings::getSettings()->{'database'}->{'db_name'};
+            $this->db_charset = Settings::getSettings()->{'database'}->{'db_charset'};
+            $this->db_username = Settings::getSettings()->{'database'}->{'db_username'};
+            $this->db_password = Settings::getSettings()->{'database'}->{'db_password'};
+        }
 
         try {
-            //$dsn = "mysql:host=$this->db_host;dbname=$this->db_name;port=$this->db_port;charset=$this->db_charset";
-            //$dsn = "mysql:host=$this->db_host;dbname=$this->db_name;charset=$this->db_charset";
             $dsn = "mysql:host=$this->db_host;dbname=$this->db_name;charset=$this->db_charset";
             $this->db = new PDO($dsn, "$this->db_username", "$this->db_password");
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -55,7 +59,8 @@ class Database
         return $request->fetchAll();
     }
 
-    public function execute($statement, $variables){
+    public function execute($statement, $variables)
+    {
         $request = $this->db->prepare($statement);
         $request->execute($variables);
     }
@@ -71,7 +76,8 @@ class Database
         $request->execute();
     }
 
-    public function getRequest(){
+    public function getRequest()
+    {
         return $this->request();
     }
 
--- src/test/Queries.php
+++ src/test/Queries.php
@@ -9,20 +9,20 @@ class Queries extends TestCase
 
     public function testInsertUser()
     {
-        $db = new Database();
+        $db = new Database("mysql", "blog_io", "utf8", "root", "mysql");
         $user = new User();
         $db->execute("
         CREATE TABLE `user` (
-            `id` int(11) NOT NULL AUTO_INCREMENT,
+            `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
             `first_name` varchar(50) NOT NULL,
             `last_name` varchar(50) NOT NULL,
             `email` varchar(100) NOT NULL,
             `password` varchar(255) NOT NULL,
             `level` int(2) NOT NULL DEFAULT '1'
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-        ",null);
+        ", null);
         $user->createUser($db, 'Frederic', 'Sananes', 'fsananes@test.com', sha1('1234'));
-        $response = $db->query("SELECT * FROM user",null)->fetchAll();
+        $response = $db->query("SELECT * FROM user", null)->fetchAll();
         $this->assertEquals(1, $response[0]['level']);
         $this->assertEquals("Frederic", $response[0]['first_name']);
         $this->assertEquals("Sananes", $response[0]['last_name']);
